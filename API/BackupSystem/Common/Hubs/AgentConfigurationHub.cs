f5243ed40279c01abbbcc97502e7b7cf7d2c8bc5
Jeronimo Garcia Delgado <JeronimoGarcia.Delgado@adeccogroup.com>
2023-12-18 10:58:11 +0100

    [Ref #6] Create Hub to use SignalR

---
+++ API/BackupSystem/Common/Hubs/AgentConfigurationHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BackupSystem.Common.Hubs
+{
+    public class AgentConfigurationHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            string connectionId = Context.ConnectionId;
+
+            await Clients.All.SendAsync("ReceiveConfiguration", $"{Context.ConnectionId} has joined.");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            string connectionId = Context.ConnectionId;
+
+            await Clients.All.SendAsync("ReceiveConfiguration", $"{Context.ConnectionId} has joined.");
+        }
+
+        public async Task SendConfiguration(string msg)
+        {
+            await Clients.All.SendAsync("ReceiveConfiguration", msg);
+        }
+    }
+}
