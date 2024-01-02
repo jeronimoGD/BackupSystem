e7b2bb4b347191912756ca0ef2639c577217ae6a
Jeronimo Garcia Delgado <JeronimoGarcia.Delgado@adeccogroup.com>
2024-01-02 15:45:59 +0100

    [Fix #12] Use IHostedService and Timers to manage scheduled tasks that will invoke the back ups.

---
+++ API/BackUpAgent/Common/Enums/PeridicityEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackUpAgent.Common.Enums
+{
+    public enum Periodicity
+    {
+        Daily,
+        Weekly,
+        TwoWeeks,
+        Monthly
+    }
+}
---
+++ API/BackUpAgent/Common/Interfaces/ScheduledTasks/IBackUpScheduler.cs
@@ -0,0 +1,16 @@
+﻿using BackUpAgent.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackUpAgent.Common.Interfaces.ScheduledTasks
+{
+    public interface IBackUpScheduler
+    {
+        void AddBackgroundTask(BackUpConfiguration configuration);
+        void CancelTask(string taskName);
+        Task StartAllConfiguredBackUpsAsync(List<BackUpConfiguration> backUpConfigurations);
+    }
+}
